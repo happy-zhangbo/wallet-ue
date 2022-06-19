f3b998b943ba9f2c3f213777a536c58ef7bae51f
854008949@qq.com <854008949@qq.com>
2022-06-19 22:29:28 +0800

    Update walletconnect.js desc Update Demo

--- demo.js
+++ demo.js
@@ -1,13 +1,14 @@
 const vault = require("./common/vault")
 const utils = require("./common/utils")
-
+const Web3Utils = require('web3-utils');
 
 async function main(){
     // const data = await utils.createWallet()
     // console.log(data);
 
-    const data = await vault.read();
-    console.log(data);
+    const asciiToHex = Web3Utils.asciiToHex("ABC");
+    console.log(asciiToHex);
+    console.log(asciiToHex.padEnd(66,'0'));
 }
 
 main()
